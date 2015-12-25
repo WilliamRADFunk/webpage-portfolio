58c60dabd25639cc90b1b44027c893c6817468f2
William Funk <contact@WilliamRADFunk.com>
2015-12-25 01:29:12 -0500

    Resolved issue where accordion would displace the screen location on the document.body that was disorienting.

--- js/scripts.js
+++ js/scripts.js
@@ -3,26 +3,20 @@ function jumpTo(coords)
 	var offset = (coords !== null && coords !== undefined) ? coords : document.getElementById("main-nav").offsetHeight - 2;
 	setTimeout(function()
 	{
-		if(window.innerWidth > 767)
-		{
-			window.scrollBy(0, -offset);
-		}
+		if(window.innerWidth > 767) window.scrollBy(0, -offset);
 		else
 		{
 			var hamburgerOffset = (coords !== null && coords !== undefined) ? 0 : document.getElementById("hamburger-nav").offsetHeight;
 			window.scrollBy(0, -(offset - hamburgerOffset - 16));
 		}
-	}, 100);
+	}, 0);
 }
 function setPanelToTop(elem)
 {
-	var panel = document.getElementById(elem);
-	var position = panel.getBoundingClientRect();
-	console.log(position.top);
 	setTimeout(function()
 	{
-		console.log("Page Top Before: " + document.body.scrollTop);
-		document.body.scrollTop = panel.scrollTop;
-		console.log("Page Top After: " + document.body.scrollTop);
-	}, 100);
+		var offset = document.getElementById(elem).getBoundingClientRect();
+		window.scrollBy(0, offset.top);
+		jumpTo();
+	}, 400);
 }
