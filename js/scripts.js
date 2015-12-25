7c9cbce55fb4adef7bec287f3d38c652c528370d
William Funk <contact@WilliamRADFunk.com>
2015-12-24 23:56:28 -0500

    Resolved JumpTo navigation offset when dealing with the hamburger menu nav of smaller, mobile screens.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,28 @@
-function jumpTo()
+function jumpTo(coords)
 {
-	var offset = document.getElementById("main-nav").offsetHeight - 2;
+	var offset = (coords !== null && coords !== undefined) ? coords : document.getElementById("main-nav").offsetHeight - 2;
 	setTimeout(function()
 	{
-		window.scrollBy(0, -offset);
+		if(window.innerWidth > 767)
+		{
+			window.scrollBy(0, -offset);
+		}
+		else
+		{
+			var hamburgerOffset = (coords !== null && coords !== undefined) ? 0 : document.getElementById("hamburger-nav").offsetHeight;
+			window.scrollBy(0, -(offset - hamburgerOffset - 16));
+		}
+	}, 100);
+}
+function setPanelToTop(elem)
+{
+	var panel = document.getElementById(elem);
+	var position = panel.getBoundingClientRect();
+	console.log(position.top);
+	setTimeout(function()
+	{
+		console.log("Page Top Before: " + document.body.scrollTop);
+		document.body.scrollTop = panel.scrollTop;
+		console.log("Page Top After: " + document.body.scrollTop);
 	}, 100);
 }
